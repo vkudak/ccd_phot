a22663a7a5bae34ef271d0b8cf6c7b469cf4ddf9
Viktor <vkudak@users.noreply.github.com>
2020-07-22 13:44:38 +0300

    Add files via upload
    initial commit

---
+++ sat_phot2.py
@@ -0,0 +1,217 @@
+from astropy.stats import sigma_clipped_stats
+from astropy.io import fits
+import numpy as np
+from photutils import CircularAperture, CircularAnnulus
+from photutils import aperture_photometry
+from sp_utils import *
+import math
+import glob
+import sys
+import os
+import configparser
+
+
+def calc_mag(flux, el, rg, A, k):
+    m_inst = -2.5 * math.log10(flux)
+    mz = 1 / (math.cos(math.pi / 2 - math.radians(el)))  # 1/cos(90-el)
+    mr = -5 * math.log10(rg / 1000.0)
+    mzr = k * mz
+
+    mag = A + m_inst + mzr + mr
+    return mag
+
+
+path = sys.argv[1]
+
+config = configparser.ConfigParser()
+config.read(path + '//config_sat.ini')
+if os.path.isfile(path + '//config_sat.ini'):
+    try:
+        cospar = config['NAME']['COSPAR']
+        norad = config['NAME']['NORAD']
+        name = config['NAME']['NAME']
+
+        tle_file = config['TLE']['tle_file']
+
+        A = float(config['STD']['A'])
+        k = float(config['STD']['k'])
+
+        r_ap = float(config['APERTURE']['ap'])
+        an_in = float(config['APERTURE']['an_in'])
+        an_out = float(config['APERTURE']['an_out'])
+
+    except Exception as E:
+        print("Error in inin file\n", E)
+        sys.exit()
+else:
+    print ("Error. Cant find config_sat.ini in " + path + '//config_sat.ini')
+
+tle_list = get_tle(tle_file)
+
+
+list = os.listdir(path)
+fl = []
+for fn in list:
+    f = fn.split('.')
+    if f[-1] in ['FIT', 'FITS', 'fit', 'fits']:
+        fl.append(l)
+
+fl.sort()
+
+debug = True
+
+if debug:
+    if os.path.isdir(path + "//fig"):
+        files = glob.glob(path + "//fig//*.*")
+        for f in files:
+            os.remove(f)
+    else:
+        os.makedirs(path + "//fig")
+
+fl = fl[:5]   # first 10 files  ------  file # -1
+
+
+# fl = ["Capture_00016.fits"]
+
+fr = open(path + "//result.txt", "w")
+# fr.write("     Date              UT                   X                 Y                Xerr          Yerr                 Flux                filename\n")
+
+for fit_file in fl:
+    print ("filename=", fit_file)
+    hdu = fits.open(path + "//" + fit_file)[0]
+    data = hdu.data
+    header = hdu.header
+    date_time = header.get('DATE-OBS')
+    if date_time == '0001-01-01T00:00:00.0000000':
+        date_time = header.get('DATE-END')  # NO GPS time data !!!!!!!!!!!!
+    width = int(header.get('NAXIS1'))
+    height = int(header.get('NAXIS2'))
+    t_x, t_y = None, None
+    try:
+        t_x = float(header.get('OBJX'))
+        t_y = float(header.get('OBJY'))
+    except Exception:
+        pass
+
+    mean, median, std = sigma_clipped_stats(data, sigma=3.0)
+
+    if fit_file == fl[0]:  # make header--------------------------------------------------------------------
+        El, Rg, Az, name, nor, cosp, tle_lines = calc_from_tle(tle_list, date_time, cospar, norad, name)
+        fr.write("# TLE:\n")
+        fr.write("# %s\n" % tle_lines[0])
+        fr.write("# %s\n" % tle_lines[1])
+        fr.write("# %s\n" % tle_lines[2])
+        fr.write("# NORAD  = %s\n" % nor)
+        fr.write("# COSPAR = %s\n" % cosp)
+        fr.write("# NAME   = %s\n" % name)
+
+        fr.write("   Date       UT              X          Y         Xerr      Yerr          Flux         mag      filename\n")
+
+    ##################################
+
+    # BEGIN----
+    ph_image = data
+    data = data - median
+
+    if t_x is not None:  # target from header
+        target = [t_x, height - t_y]
+        x0, y0 = int(target[0]), int(target[1])
+        error = [0, 0]
+        target.append(error[0])
+        target.append(error[1])
+
+        fig_name = path + "//fig//" + fit_file + "_man.png"
+        target = fit_m(data, x0, y0, gate=20, debug=debug, fig_name=fig_name)
+
+    else:  # SEARCH target
+        target = None
+        fig_name = path + "//fig//" + fit_file + ".png"
+        try:
+            target = fit_m(data, x0, y0, gate=20, debug=debug, fig_name=fig_name)
+
+            target_original = target
+
+            x0, y0 = int(target[0]), int(target[1])
+            err = target[2]
+            if (err > 0.9) and (err < 1.5):
+                target = fit_m(data, x0, y0, gate=10, debug=debug, fig_name=fig_name, centring=True)
+            elif err > 1.5:
+                target = fit_m(data, x0, y0, gate=20, debug=debug, fig_name=fig_name)
+
+                x0, y0 = int(target[0]), int(target[1])  # one more time with smaller window
+                target = fit_m(data, x0, y0, gate=10, debug=debug, fig_name=fig_name, centring=True)
+
+            err = target[2]
+            if err > 1:
+                target = target_original
+                x0, y0 = int(target[0]), int(target[1])
+            else:
+                x0, y0 = int(target[0]), int(target[1])
+            print ("final=", x0, y0)
+        except Exception as E:
+            print(E)
+            print("Error - curve_fit failed\n")
+
+    # ------------------------------------ PHOTOMETRY-------------------------------------------------
+    if (target) and (target[2] < 2):
+        positions = target[:2]
+        aperture = CircularAperture(positions, r=r_ap)
+        annulus_aperture = CircularAnnulus(positions, r_in=an_in, r_out=an_out)
+
+        # from photutils import EllipticalAperture, EllipticalAnnulus
+        # aperture = EllipticalAperture(positions, a=4, b=3, theta=-0.25*np.pi)
+        # annulus_aperture = EllipticalAnnulus(positions, a_in=12., a_out=14., b_out=8., theta=-0.25*np.pi)
+
+        apers = [aperture, annulus_aperture]
+        # print (apers)
+        phot_table = aperture_photometry(ph_image, apers)
+        for col in phot_table.colnames:
+            phot_table[col].info.format = '%.8g'  # for consistent table output
+        # print(phot_table)
+
+        bkg_mean = phot_table['aperture_sum_1'] / annulus_aperture.area
+        bkg_sum = bkg_mean * aperture.area
+        final_sum = phot_table['aperture_sum_0'] - bkg_sum
+        phot_table['residual_aperture_sum'] = final_sum
+        phot_table['residual_aperture_sum'].info.format = '%.8g'  # for consistent table output
+
+        z = 0
+        if len(phot_table) > 1:
+            if math.isnan(phot_table['residual_aperture_sum'][z]):
+                z = 1
+            for i in range(0, len(phot_table)):
+                if not math.isnan(phot_table['residual_aperture_sum'][i]):
+                    if phot_table['residual_aperture_sum'][i] > phot_table['residual_aperture_sum'][z]:
+                        z = i
+
+        date, time = date_time.split("T")
+        if len(target) == 4:
+            xerr, yerr = target[2], target[3]
+        else:
+            xerr, yerr = 9, 9
+
+        if (xerr is np.inf) or (yerr is np.inf):
+            xerr, yerr = 8, 8
+
+        flux = phot_table['residual_aperture_sum'][z]
+        # print (phot_table)
+
+        El, Rg, Az, name, nor, cosp, tle_lines = calc_from_tle(tle_list, date_time, cospar, norad, name)
+        mag = calc_mag(flux, El, Rg, A, k)
+        # fr.write("%s %s    %8.5f  %8.5f  %8.5f  %8.5f  %12.5f   %s\n" % (date, time[:12], phot_table['xcenter'][z].value, phot_table['ycenter'][z].value, xerr, yerr, flux, fit_file))
+        fr.write("%s %s    %8.5f  %8.5f  %8.5f  %8.5f     %s   %8.5f   %s\n" % (date, time[:12], phot_table['xcenter'][z].value, phot_table['ycenter'][z].value, xerr, yerr, '{:010.4f}'.format(flux), mag, fit_file))
+
+        # PLOT GENERAL FIT with apperture
+        # import matplotlib.pyplot as plt
+        # from matplotlib.patches import Circle
+        # from astropy.visualization import SqrtStretch, LogStretch
+        # from astropy.visualization.mpl_normalize import ImageNormalize
+        # fig, ax = plt.subplots()
+        # ax.imshow(data, origin='lower')
+        # norm = ImageNormalize(stretch=LogStretch())
+        # plt.imshow(data, cmap='Greys', origin='lower', norm=norm)
+        # # ax.scatter(target[0], target[1], s=20, c='red', marker='x')
+        # circle = Circle((target[0], target[1]), 8, facecolor='none', edgecolor='red', linewidth=1, fill=False)
+        # ax.add_patch(circle)
+        # plt.show()
+        # plt.close('all')
---
+++ sp_utils.py
@@ -0,0 +1,236 @@
+import numpy as np
+# import astropy.io.fits as fits
+import os
+# from astropy.stats import mad_std
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+import ephem
+from datetime import datetime
+import math
+from astropy import units as u
+from astroquery.vizier import Vizier
+import astropy.coordinates as coord
+
+
+def lsqFit(y, x):
+    '''
+    y=ax+c
+    Return a, c, residual
+    '''
+    x = np.array(x)
+    y = np.array(y)
+    A = np.vstack([x, np.ones(len(x))]).T
+    res = []
+    # linearly generated sequence
+    if y != []:
+        wb = np.linalg.lstsq(A, y)  # obtaining the parameters
+        a, c = wb[0]
+        #residual = wb[1][0]
+    #else:
+    #    residual = 999
+    for i in range(0, len(x)):
+        res.append(abs(y[i] - (a * x[i] + c)))
+    res = np.array(res)
+    res_max = np.max(res)
+    res_min = np.min(res)
+    if res_max > abs(res_min):
+        ind = np.argmax(res)
+    else:
+        ind = np.argmin(res)
+        res_max = abs(min)
+    return a, c, res_max, ind
+
+
+def get_from_NOMAD(RA, DEC, w="0d60m", h="30m", radius=None, Filter={'Rmag': '<13'}):
+    center = coord.SkyCoord(ra=RA, dec=DEC, unit=(u.deg, u.deg), frame='icrs')
+    Vizier.ROW_LIMIT = 5000
+    if radius:
+        table = Vizier.query_region(center, width=w, radius=radius, catalog=["NOMAD"], column_filters=Filter)
+    else:
+        table = Vizier.query_region(center, width=w, height=h, catalog=["NOMAD"], column_filters=Filter)
+    return table
+
+
+def del_var(table, Filter={'Vmag': '<10'}):
+    table_final = table
+    Vizier.ROW_LIMIT = 5000
+    i = 0
+    for row in table_final:
+        ra = row["RAJ2000"]
+        dec = row["DEJ2000"]
+        c = coord.SkyCoord(ra=ra * u.degree, dec=dec * u.degree, frame='icrs')
+        var_table = Vizier.query_region(c, radius='0d0m5s', catalog=["GCVS"], column_filters=Filter)  # , catalog=["GCVS"]
+        var_table_len = len(var_table)
+        if var_table_len > 0:
+            table_final.remove_row(i)
+            # print ("delete", row)
+        i = i + 1
+    return table_final
+
+
+def gaussian(xycoor, x0, y0, sigma, amp):
+    '''This Function is the Gaussian Function'''
+
+    x, y = xycoor  # x and y taken from fit function.  Stars at 0, increases by 1, goes to length of axis
+    A = 1 / (2 * sigma**2)
+    eq = amp * np.exp(-A * ((x - x0)**2 + (y - y0)**2))  # Gaussian
+    return eq
+
+
+def fit(image, mf=None):
+    # med = np.median(image)
+    # image = image-med
+    # image = image[0,0,:,:]
+
+    # max_index = np.where(image >= np.max(image))
+    # x0 = max_index[1]  # Middle of X axis
+    # y0 = max_index[0]  # Middle of Y axis
+
+    x0 = int(image.shape[1] / 2)
+    y0 = int(image.shape[0] / 2)
+    # print (x0, y0)
+
+    x = np.arange(0, image.shape[1], 1)  # Stars at 0, increases by 1, goes to length of axis
+    y = np.arange(0, image.shape[0], 1)  # Stars at 0, increases by 1, goes to length of axis
+    xx, yy = np.meshgrid(x, y)  # creates a grid to plot the function over
+
+    sigma = np.std(image)  # The standard dev given in the Gaussian
+    amp = np.max(image)  # amplitude
+    guess = [x0, y0, sigma, amp]  # The initial guess for the gaussian fitting
+
+    low = [0, 0, 0, 0]  # start of data array
+    # Upper Bounds x0: length of x axis, y0: length of y axis, st dev: max value in image, amplitude: 2x the max value
+    upper = [image.shape[0], image.shape[1], np.max(image), np.max(image) * 2]
+    # upper = [image.shape[0], image.shape[1], sigma * 100.0, np.max(image) * 0.5]
+    bounds = [low, upper]
+
+    params, pcov = curve_fit(gaussian, (xx.ravel(), yy.ravel()), image.ravel(), p0=guess, bounds=bounds, maxfev=mf)  # optimal fit.  Not sure what pcov is.
+
+    # par, cov, infodict, mesg, ier = optimize.leastsq(residuals, a_guess, args=(x,y),full_output=True)
+    # params, pcov = curve_fit(gaussian, (xx.ravel(), yy.ravel()), image.ravel(), p0=guess, bounds=bounds)  #optimal fit.  Not sure what pcov is.
+    # print("here")
+    return params, pcov
+
+
+def plotting(image, params, save=False, filename=None, par=None, gate=None):
+    try:
+        fig, ax = plt.subplots()
+        ax.imshow(image, origin='lower')
+        ax.scatter(params[0], params[1], s=20, c='red', marker='x')
+        circle = Circle((params[0], params[1]), params[2], facecolor='none', edgecolor='red', linewidth=1)
+
+        ax.add_patch(circle)
+
+        if save:
+            filename, file_extension = os.path.splitext(filename)
+            filename = filename + "_g%i" % gate + file_extension
+            plt.title("parameters= %s \nerrors=%s \ngate=%s" % (par[:2], par[2:], gate), pad=-50, fontsize=10)
+            plt.savefig(filename)
+        else:
+            plt.show()
+        plt.close('all')
+    except Exception as E:
+        print (E)
+
+
+def fit_m(image, x0, y0, gate, debug, fig_name=None, centring=False, silent=False):
+    # gate_x = gate_y
+    data_fit = image[y0 - gate:y0 + gate, x0 - gate:x0 + gate]
+    # plt.imshow(data_fit, cmap='Greys', origin='lower')
+    # plt.show()
+    if centring:
+        if not silent:
+            print ("centring...")
+        # ------------------------------------------------------------------ find brighter pixel and center it!!!!
+        bx0, by0 = np.unravel_index(data_fit.argmax(), data_fit.shape)
+        if not silent:
+            print ("bx, by=", bx0, by0)
+
+        sx = by0 - gate
+        sy = bx0 - gate
+
+        data_fit = image[y0 - gate + sy:y0 + gate + sy, x0 - gate + sx:x0 + gate + sx]
+        # ----------------------------------------------------------------------------------------
+    par, pcov = fit(data_fit)
+    err = np.sqrt(np.diag(pcov))
+
+    target = [x0 - gate + par[0], y0 - gate + par[1], err[0], err[1]]
+    if debug:
+        plotting(data_fit, par, save=True, filename=fig_name, par=target, gate=gate)
+    return target
+
+
+def get_tle(tle_path):
+    TLE_list = []
+    ephf = open(tle_path, 'r')
+    i = 1
+    tle1 = []
+    for l in ephf:
+        if l != '':
+            tle1.append(l.strip())
+            i = i + 1
+            if i > 3:
+                TLE_list.append(tle1)
+                tle1 = []
+                i = 1
+    ephf.close()
+    return TLE_list
+
+
+def calc_from_tle(TLE_list, date_time, COSPAR, NORAD, NAME):
+    if COSPAR == '' and NORAD == '' and NAME == '':
+        return None
+    else:
+        try:
+            tle = []
+            for i in range(0, len(TLE_list)):
+                l2 = TLE_list[i][1].split()
+                cosp = l2[2]
+                nor = l2[1]
+                name = TLE_list[i][0]
+                if cosp == COSPAR:
+                    c = l2[2]
+                    no = l2[1][:-1]
+                    n = TLE_list[i][0]
+                    tle = TLE_list[i]
+            if tle == []:
+                for i in range(0, len(TLE_list)):
+                    l2 = TLE_list[i][1].split()
+                    nor = l2[1][:-1]
+                    # print 'nor=', nor
+                    if nor == NORAD:
+                        c = l2[2]
+                        no = l2[1][:-1]
+                        n = TLE_list[i][0]
+                        tle = TLE_list[i]
+            if tle == []:
+                for i in range(0, len(TLE_list)):
+                    l2 = TLE_list[i][1].split()
+                    name = TLE_list[i][0]
+                    if name == NAME:
+                        c = l2[2]
+                        no = l2[1][:-1]
+                        n = TLE_list[i][0]
+                        tle = TLE_list[i]
+            if len(tle) > 0:
+                # Calculating  El, Rg
+                station = ephem.Observer()
+                station.lat = '48.5635505'
+                station.long = '22.453751'
+                station.elevation = 231.1325
+                sat = ephem.readtle(tle[0], tle[1], tle[2])
+                try:
+                    station.date = datetime.strptime(date_time[:-1], "%Y-%m-%dT%H:%M:%S.%f")
+                except Exception:
+                    print ("Error - Wrong date time format...")
+
+                sat.compute(station)
+                el = math.degrees(sat.alt)
+                rg = sat.range / 1000.0  # km
+                az = math.degrees(sat.az)
+                return el, rg, az, n, no, c, tle
+            else:
+                print('Cant find TLE for such satellite!')
+        except Exception as ex:
+            print("Error occurred. Probably wrong TLE file. \n Error = " + ex.message)
